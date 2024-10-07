525ec8bc3449bdff14a4e9957683d68b2573388a
Bui Chi Vi <104517891+buichivi@users.noreply.github.com>
2024-10-07 22:30:16 +0700

    feat(task 1): implement function to find the most frequent string lengths

--- Task_1/main.js
+++ Task_1/main.js
@@ -3,5 +3,17 @@
  *  @param { string[] } arr
  *  @return { string[] }
  */
+export default function mostFrequentStrings(arr) {
+  const lengthCounts = arr.reduce((acc, item) => {
+    const length = item.length;
+    const count = (acc.get(length) || 0) + 1;
+    acc.set(length, count);
+    return acc;
+  }, new Map());
 
-export default function mostFrequentStrings(arr) {}
+  const maxCount = Math.max(...Array.from(lengthCounts.values()));
+  const mostFrequentLength = Array.from(lengthCounts.keys()).filter(
+    (length) => lengthCounts.get(length) === maxCount
+  );
+  return arr.filter((item) => mostFrequentLength.includes(item.length));
+}
