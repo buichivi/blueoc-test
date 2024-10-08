d3808f33a54498bba8825876873f6c1f802cbd19
Bui Chi Vi <104517891+buichivi@users.noreply.github.com>
2024-10-08 09:33:34 +0700

    chore(task 3): initialize React-Redux application structure

---
+++ Task_3/src/redux/postsSlice.ts
@@ -0,0 +1,48 @@
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// type Post = {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   body: string;
+// };
+
+// type PostState = {
+//   items: Post[];
+//   status: 'idle' | 'loading' | 'succeeded' | 'failed';
+//   error: string | null;
+// };
+
+// const initialState: PostState = {
+//   items: [],
+//   status: 'idle',
+//   error: null,
+// };
+
+// export const fetchPosts = createAsyncThunk<Post[]>('posts/fetchPosts', async () => {
+//   const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+//   return res.data;
+// });
+
+// const postsSlice = createSlice({
+//   name: 'posts',
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchPosts.pending, (state) => {
+//         state.status = 'loading';
+//       })
+//       .addCase(fetchPosts.fulfilled, (state, action) => {
+//         state.status = 'succeeded';
+//         state.items = action.payload;
+//       })
+//       .addCase(fetchPosts.rejected, (state, action) => {
+//         state.status = 'failed';
+//         state.error = action.error.message || 'Failed to fetch posts';
+//       });
+//   },
+// });
+
+// export default postsSlice.reducer;
